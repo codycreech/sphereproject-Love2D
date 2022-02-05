e9d27bd60e49596a390e378fb2ffaef54688fb92
Cody Creech <codycreech@me.com>
2022-02-05 12:18:48 -0500

    Code cleanup

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ function love.load()
   love.graphics.setCanvas(canvas_grid)
     love.graphics.clear()
     love.graphics.setBlendMode("alpha", "premultiplied")
-    --love.graphics.setColor(0, 0.5, 0.75)
+    
     drawGrid()
 
   love.graphics.setCanvas()
@@ -43,38 +43,6 @@ function love.load()
 
   love.graphics.setCanvas()
 
-  -- items = {}
-  -- for x = 1, #nodes, 1 do
-  --   table.insert(items, x)
-  -- end
-  -- items2 = {}
-  -- -- for x = 1,12 do
-  -- --   table.insert(items2, x)
-  -- -- end
-  -- for i,v in ipairs(grid) do
-  --   for j,v2 in ipairs(v) do
-  --     for k,v3 in ipairs(grid_pattern) do
-  --       -- print('Grid['..i..','..j..']')
-  --       -- print('Pattern['..k..']')
-  --       -- love.timer.sleep(1)
-  --     end
-  --   end
-  -- end
-
-  -- local p = grid[4][10]
-  -- for i,v in ipairs(p) do
-  --   for j,v2 in ipairs(v) do
-  --     print(v2)
-  --   end
-  -- end
-
-  -- combo = {value = 1, items = items}
-  -- combo2 = {value = 1, items = items2}
-  -- combo3 = {value = 1, items = items3}
-
-  -- for i = 1, #nodes, 1 do
-  --   table.insert(nodes[i], {color = {1,1,0}})
-  -- end
   red = 128/255
   green = 181/255
   blue = 64/255
@@ -92,43 +60,11 @@ function love.update(dt)
   sliderG:update()
   sliderB:update()
 
-  -- ui:frameBegin()
-  -- if ui:windowBegin('Nodes', 1050, 160, 200, 175, 'border', 'title') then
-  --   ui:layoutRow('dynamic', 30, 1)
-  --   ui:label('Nodes:')
-  --   if ui:combobox(combo, combo.items) then
-  --     print(combo.items[combo.value])
-  --   end
-  --
-  --   ui:layoutRow('dynamic', 30, 1)
-  --   ui:label('Set Color of Selected Node:')
-  --   ui:layoutRow('dynamic', 30, 2)
-  --   if ui:button('Set') then
-  --     nodes[combo.value].color = {red, green, blue}
-  --   end
-  -- end
-  -- ui:windowEnd()
-  -- ui:frameEnd()
-  --
-  -- ui_grid:frameBegin()
-  -- if ui_grid:windowBegin('Grid', 1050, 350, 200, 200, 'border', 'title') then
-  --   ui_grid:layoutRow('dynamic', 30, 3)
-  --   ui_grid:label('Grid:')
-  --   if ui_grid:combobox(combo2, combo2.items) then
-  --     print(combo2.items[combo2.value])
-  --   end
-  -- end
-  -- ui_grid:windowEnd()
-  -- ui_grid:frameEnd()
-
   delta = dt
 
-  -- canvas_sphere:renderTo(function()
   love.graphics.setCanvas(canvas_sphere)
     love.graphics.clear()
 
-    -- love.graphics.setColor(nodes[1].color)
-    --draw sphere
     nextNode = 14
     grid_row = 1
 
@@ -139,14 +75,7 @@ function love.update(dt)
       if grid_row > 16 then
         grid_row = 1
       end
-      -- if i % 2 == 0 then
-      --   love.graphics.setColor(1,0,0)
-      -- else
-      --   love.graphics.setColor(1,1,1)
-      -- end
-      -- if i == 452 then
-      --   love.graphics.setColor(red, green, blue)
-      -- end
+
       local p = grid[1][1]
 
       if node == nextNode then
@@ -154,43 +83,20 @@ function love.update(dt)
       else
         love.graphics.setColor(nodes[i].color)
       end
-      -- love.graphics.setColor(nodes[i].color)
-      -- love.graphics.setPointSize(3)
+
   		love.graphics.points(node.x*canvas_sphere_scale+centerX, node.y*canvas_sphere_scale+centerY)
       love.graphics.circle('fill', node.x*canvas_sphere_scale+centerX, node.y*canvas_sphere_scale+centerY, radius)
-      -- love.graphics.print(i, node.x*scale+centerX, node.y*scale+centerY)
-      -- love.graphics.reset()
+
       if mousex >= node.x*canvas_sphere_scale+centerX - (radius+margin) and mousex <= (radius+margin) + node.x*canvas_sphere_scale+centerX and
       mousey >= node.y*canvas_sphere_scale+centerY - (radius+margin) and mousey <= (radius+margin) + node.y*canvas_sphere_scale+centerY then
         love.graphics.setColor(1, 1, 1)
-        --love.graphics.draw(love.graphics.newText(font1, i), mousex, mousey)
         love.graphics.print(i, mousex+15, mousey+15)
       end
-      -- nextNode = nextNode + 15
-      -- grid_row = grid_row + 1
+
   	end
 
-    -- if drawEdges == true then
-    --   love.graphics.setColor(1, 1, 1)
-  	-- 	for n = 1, #edges, 1 do
-  	-- 		n0 = edges[n][1]
-  	-- 		n1 = edges[n][2]
-  	-- 		node00 = nodes[n0]
-  	-- 		node11 = nodes[n1]
-  	-- 		love.graphics.line(node00.x*scale+centerX, node00.y*scale+centerY, node11.x*scale+centerX, node11.y*scale+centerY)
-  	-- 		print(n0)
-  	-- 	end
-  	-- end
-
-    -- love.graphics.setLineWidth(1.75)
-    -- love.graphics.setLineStyle('smooth')
-    -- love.graphics.line(nodes[463].x*scale+centerX, nodes[463].y*scale+centerY, nodes[477].x*scale+centerX, nodes[477].y*scale+centerY)
-
-
     love.graphics.reset()
-    --drawPatternToSphere()
 
-  -- end);
   love.graphics.setCanvas()
 
   if love.mouse.isDown('1') then
@@ -214,8 +120,6 @@ function love.draw()
   love.graphics.reset()
 
   love.graphics.draw(canvas_sphere, 2, 2)
-
-  -- drawSquare(pattern, 1, nodes[451], scale, centerX, centerY)
 
   ui:draw()
   ui_grid:draw()
@@ -274,19 +178,6 @@ end
 function love.wheelmoved(x, y)
 	ui:wheelmoved(x, y)
 end
-
--- function rotateZ3D(alpha)
--- 	sin = math.sin(alpha)
--- 	cos = math.cos(alpha)
--- 	for a = 1, #nodes, 1 do
--- 		node = nodes[a]
--- 		x = node.x
--- 		y = node.y
--- 		node.x = (x * cos - y * sin)
--- 		node.y = (y * cos + x * sin)
--- 	end
--- end
-
 
 function rotateY3D(alpha)
 	sin = math.sin(alpha)
